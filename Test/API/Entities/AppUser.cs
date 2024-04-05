75d57d795f335749ca30c6279f010a6575b331ef
tiennd03 <120414157+tiennd03@users.noreply.github.com>
2024-04-05 11:15:35 +0700

    hello

---
+++ Test/API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string mail { get; set; }
+        public decimal salary { get; set; }
+    }
+}
